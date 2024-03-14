5e367e64bdc9e4c179b528c039509fc458914aaf
Cheobaby <noelmtzramirez1.6@gmail.com>
2024-03-14 13:43:57 -0600

    Mis lugares start

--- Cliente/App.js
+++ Cliente/App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MisLugares from './Screens/MisLugares';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MisLugares></MisLugares>    
   );
 }
 
---
+++ Cliente/Screens/MisLugares.js
@@ -0,0 +1,86 @@
+import {StyleSheet, Text, View, StatusBar, Button,TouchableOpacity,Image,Dimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import * as React from 'react';
+import MapView, {Marker,Polyline} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+const { width, height } = Dimensions.get('window');
+
+const MisLugares=()=>{
+    const [origin,setOrigin]=React.useState({
+        latitude:20.581436,
+        longitude:-100.390633,
+      });
+    const [destination,setDestination]=React.useState({
+        latitude:20.589386,
+        longitude:-100.410147,
+    });
+    return(
+        <View style={styles.container}>
+            <View style={styles.title}>
+                <TouchableOpacity>
+                    <Icon name="arrow-back" size={39} color="white" 
+                    style={{marginStart:20}}/>                    
+                </TouchableOpacity>
+                <Text style={{color:'white',fontWeight:'bold',fontSize:35,marginStart:20}}>Mis lugares</Text>
+            </View>
+            <View style={styles.body}>
+                <MapView style={styles.map}
+                    initialRegion={{
+                    latitude: origin.latitude,
+                    longitude: origin.longitude,
+                    latitudeDelta:0.09,
+                    longitudeDelta:0.06
+                    }}
+                >
+                <Marker
+                    draggable
+                    coordinate={origin}
+                    onDragEnd={(direction)=>{setOrigin(direction.nativeEvent.coordinate)}}
+                />
+                <Marker
+                    draggable
+                    coordinate={destination}
+                    onDragEnd={(direction)=>{setDestination(direction.nativeEvent.coordinate)}}
+                />
+                <MapViewDirections
+                    origin={origin}
+                    destination={destination}                    
+                />
+                <Polyline
+                    coordinates={[origin,destination]}
+                    strokeColor='black'
+                    strokeWidth={3}
+                />
+                </MapView>
+            </View>
+            
+        </View>
+    );    
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'flex-start',
+        alignItems:'center',                        
+    },
+    title:{
+        backgroundColor:'#2447E5',        
+        width,
+        height:100,
+        justifyContent:'flex-start',
+        alignItems:'flex-end',  
+        flexDirection:'row',      
+    },
+    body:{        
+        width,   
+        height:350,
+    },
+    map:{
+        width,
+        height:350,        
+    },
+})
+
+export default MisLugares;
