a1bc8fd73a67c41e2c99a9b92b6c3484d8201503
HannielOlveraMata <olveramatahanniel@gmail.com>
2024-03-14 00:14:03 -0600

    hola

--- Cliente/App.js
+++ Cliente/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Auth from "./components/Auth";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Google Sign</Text>
+      <Auth/>
       <StatusBar style="auto" />
     </View>
   );
