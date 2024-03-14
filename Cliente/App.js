14c79d7a8f3c0f38d2adcfd4c8b860a2ff59e177
HannielOlveraMata <olveramatahanniel@gmail.com>
2024-03-14 17:32:53 -0600

    c

--- Cliente/App.js
+++ Cliente/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './Screens/Home';
+import RegistroScreen from './Screens/Registro';
+import MercadoPagoScreen from './Screens/Details';
+import { Provider } from 'react-redux';
+import { store } from './store'; // Importa tu tienda Redux desde el archivo store.js en la raíz de la carpeta
+import Details from './Screens/Details';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Registro" component={RegistroScreen} />
+          <Stack.Screen name="MercadoPago" component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
